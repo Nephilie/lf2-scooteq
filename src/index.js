7a45ff5e879ba59eb496dc94e6f4fe6734ab673f
FlorianJuenemann <compufloh@web.de>
2023-07-06 10:20:13 +0200

    test

--- src/index.js
+++ src/index.js
@@ -22,3 +22,36 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+const englishCode = "en-UK";
+
+const frenchCode = "es-FS";
+
+function getAboutUsLink(language){
+
+    switch (language.toLowerCase()){
+
+      case englishCode.toLowerCase():
+
+        return '/about-us';
+
+      case frenchCode.toLowerCase():
+
+        return '/-à propos de nous';
+
+    }
+
+    return '';
+
+}
+
+module.exports = getAboutUsLink;
+
+const getAboutUsLink = require("./index");
+
+test("Return about-us for english language", () => {
+
+    expect(getAboutUsLink("en-UK")).toBe("/about-us");
+
+});
